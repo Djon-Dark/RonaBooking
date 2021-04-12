6192a57781ce41a35256f5eb481317c830d4c0a3
Djon-Dark <mrak.ivan@gmail.com>
2021-04-12 16:20:39 +0200

    incorporated single open/close btn

--- ronabooking.com/RonaNEW.js
+++ ronabooking.com/RonaNEW.js
@@ -4,19 +4,30 @@ const searchBtn = document.querySelector('.searchBtn');
 const bookingForm = document.querySelector('.bookingForm');
 const closeBtn = document.querySelector('.closeBtn');
 
+
+
 burger.addEventListener('click', ()=>{
+    if(bookingForm.classList.contains('showBookingForm')){
+        bookingForm.classList.toggle('showBookingForm');
+        burger.classList.toggle('toggle');
+        return;
+    }
     burger.classList.toggle('toggle');
     navLinks.classList.toggle('showNav');
 })
 
 searchBtn.addEventListener('click', ()=>{
+    
     bookingForm.classList.toggle('showBookingForm');
     setTimeout(()=>{
         closeBtn.classList.toggle('cross');
     },200)
-    
+    if(bookingForm.classList.contains('showBookingForm')){
+        burger.classList.toggle('toggle');
+    }
 })
  
+/* OBSOLETE, INCORPORATED TO ONE OPEN/CLOSE BUTTON
 closeBtn.addEventListener('click', ()=>{
     closeBtn.classList.toggle('cross');
     setTimeout(()=>{
@@ -24,3 +35,4 @@ closeBtn.addEventListener('click', ()=>{
     },200)
     
 })
+*/
