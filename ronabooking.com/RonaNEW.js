7d1a29f7c49dba35bdae5a0e26033a8f5c0f9679
DjonDark <mrak.ivan@gmail.com>
2021-04-28 18:48:32 +0200

    mobile navdock functionality done except favorites

--- ronabooking.com/RonaNEW.js
+++ ronabooking.com/RonaNEW.js
@@ -12,6 +12,7 @@ const avatar = document.querySelector('.avatar');
 const profilePage = document.querySelector('.profile-page');
 const dockLang = document.querySelector('.dock-language');
 const dockUser = document.querySelector('.dock-user');
+const dockSearch = document.querySelector('.dock-search');
 
 //BURGER
 burger.addEventListener('click', ()=>{
@@ -39,7 +40,7 @@ burger.addEventListener('click', ()=>{
             languages.classList.toggle('display-languages');
             burger.classList.toggle('toggle');
             header.classList.toggle('open-header');
-            navLinks.classList.toggle('disabled');
+            navLinks.classList.remove('disabled');
             navLinks.classList.toggle('showNav');
         },200)
         return;
@@ -64,6 +65,17 @@ function openLanguageSelect () {
             profilePage.classList.add('disabled');
             header.classList.toggle('open-header');
         },200);
+        return;
+    }
+    if(searchBtn.classList.contains('expand-search')){
+        searchBtn.classList.remove('expand-search');
+        burger.classList.remove('toggle');
+        bookingForm.classList.remove('showBookingForm');
+        setTimeout(()=>{
+            languages.classList.toggle('display-languages');
+            profilePage.classList.add('disabled');
+            header.classList.toggle('open-header');
+        },200)
         return;
     }
     if(burger.classList.contains('toggle')){
@@ -96,6 +108,16 @@ avatar.addEventListener('click', openProfile);
 dockUser.addEventListener('click', openProfile);
 
 function openProfile(){
+    if(searchBtn.classList.contains('expand-search')){
+        searchBtn.classList.remove('expand-search');
+        burger.classList.remove('toggle');
+        bookingForm.classList.remove('showBookingForm');
+        setTimeout(()=>{
+            profilePage.classList.remove('disabled');
+            header.classList.toggle('open-header-profile');
+        },200)
+        return;
+    }
     if(burger.classList.contains('toggle')){
         header.classList.remove('open-header');
         burger.classList.remove('toggle');
@@ -115,13 +137,38 @@ function openProfile(){
         },200)
         return;
     }
+
     profilePage.classList.remove('disabled');
     header.classList.toggle('open-header-profile');
 }
 
 //SEARCH BUTTON
 searchBtn.addEventListener('click', ()=>{
+    expandSearch();
+    })
+
+function expandSearch(){
+    //close other menus if they're open
+    if(header.classList.contains('open-header')||header.classList.contains('open-header-profile')){
+        header.classList.remove('open-header');
+        header.classList.remove('open-header-profile');
+        burger.classList.remove('toggle');
+        navLinks.classList.remove('showNav');
+        languages.classList.remove('display-languages');
+        setTimeout(()=>{
+            body.classList.toggle('scroll-disabled');
+            searchBtn.classList.toggle('expand-search');
+            burger.classList.toggle('toggle');
+            bookingForm.classList.toggle('showBookingForm');
+            document.body.scrollTop = 0; // For Safari
+            document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        },200);
+        return;
+    }
     if(searchBtn.classList.contains('expand-search')){
+        searchBtn.classList.remove('expand-search');
+        bookingForm.classList.remove('showBookingForm');
+        burger.classList.remove('toggle');
         return;
     }
     body.classList.toggle('scroll-disabled');
@@ -130,6 +177,10 @@ searchBtn.addEventListener('click', ()=>{
     bookingForm.classList.toggle('showBookingForm');
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+//DOCK SEARCH BUTTON
+dockSearch.addEventListener('click',()=>{
+    expandSearch();
 })
 
 //SCROLL ACTIVATION ----> DEBOUNCATI ILI NEKI KURAC
@@ -143,3 +194,4 @@ window.onscroll = function() {
     }
     prevScrollpos = currentScrollPos;
 }
+
