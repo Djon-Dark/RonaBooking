75f85f5c5cd50c590b60e1f61697c37347ed1f0a
DjonDark <mrak.ivan@gmail.com>
2021-05-02 10:56:50 +0200

    added id dataset

--- addons/search-widget/search-widget.js
+++ addons/search-widget/search-widget.js
@@ -40,29 +40,31 @@ function populateList(obj){
     type = obj.type;
     title = obj.title;
     subtitle = obj.subtitle;
-    
-    switch (type){
-        case 'Place':
+    switch (type.toLowerCase()){//sets picture type
+        case 'place':
             typeImg = '<img src="./SVG/place.svg" alt=""></img>';
             break;
-        case 'House':
+        case 'house':
             typeImg = '<img src="./SVG/house.svg" alt=""></img>';
             break;
-        case 'Apartment':
+        case 'apartment':
             typeImg = '<img src="./SVG/apartment.svg" alt=""></img>';
             break;
-        case 'Villa':
+        case 'villa':
             typeImg = '<img src="./SVG/villa.svg" alt=""></img>';
             break;
         default:
             typeImg = '<img src="./SVG/place.svg" alt=""></img>';
     }
-
     //create element and append to list
     let x = document.createElement("LI");
     x.className = 'item';
-    x.dataset.original = title;
+    //add dataset to list item
+    x.dataset.id = obj.id;
+    x.dataset.title = title;
     x.dataset.type = type;
+    x.dataset.destination = obj.terms.destination;
+    //add dataset to list item
     x.innerHTML=(`<div class="type-img">`+`${typeImg}`+`</div>
     <div class="info"><div class="title">`+`${title}`+`</div><div class="subtitle">`+` ${subtitle}`)
     optionList.appendChild(x);
@@ -109,36 +111,171 @@ let arr1 = [
     ]
     searchWidget(arr1);
    */
+let testObject = [
+    {
+        "id": 7370,
+        "type": "house",
+        "title": "Islands",
+        "subtitle": "Islands, Cres, Istria, Cres",
+        "terms": {
+        "destination": "Islands",
+        "area": "Cres",
+        "sub_area": "Istria",
+        "place": "Cres",
+        "settlment": "ZBIŠINA",
+        "settlement_id": 7370
+        }
+        },
+        {
+        "id": 19200,
+        "type": "place",
+        "title": "Istria",
+        "subtitle": "Istria, Istra, Poreč (Parenzo)",
+        "terms": {
+        "destination": "Istria",
+        "area": null,
+        "sub_area": "Istra",
+        "place": "Poreč (Parenzo)",
+        "settlment": "BABIĆI",
+        "settlement_id": 19200
+        }
+        },
+        {
+        "id": 19201,
+        "type": "villa",
+        "title": "Istria",
+        "subtitle": "Istria, Istra, Poreč (Parenzo)",
+        "terms": {
+        "destination": "Istria",
+        "area": null,
+        "sub_area": "Istra",
+        "place": "Poreč (Parenzo)",
+        "settlment": "BRNOBIĆI",
+        "settlement_id": 19201
+        }
+        },
+        {
+        "id": 21505,
+        "type": "apartment",
+        "title": "Istria",
+        "subtitle": "Istria, Istra, ROVINJ",
+        "terms": {
+        "destination": "Istria",
+        "area": null,
+        "sub_area": "Istra",
+        "place": "ROVINJ",
+        "settlment": "ROVINJ",
+        "settlement_id": 21505
+        }
+        },
+        {
+        "id": 19202,
+        "type": "place",
+        "title": "Istria",
+        "subtitle": "Istria, Istra, Poreč (Parenzo)",
+        "terms": {
+        "destination": "Istria",
+        "area": null,
+        "sub_area": "Istra",
+        "place": "Poreč (Parenzo)",
+        "settlment": "CERJANI",
+        "settlement_id": 19202
+        }
+        },
+        {
+        "id": 21506,
+        "type": "place",
+        "title": "Istria",
+        "subtitle": "Istria, Istra, Poreč (Parenzo)",
+        "terms": {
+        "destination": "Istria",
+        "area": null,
+        "sub_area": "Istra",
+        "place": "Poreč (Parenzo)",
+        "settlment": "FUNTANA",
+        "settlement_id": 21506
+        }
+        },
+        {
+        "id": 7171,
+        "type": "place",
+        "title": "Istria",
+        "subtitle": "Istria, West coast, Istra, Buje (Buie)",
+        "terms": {
+        "destination": "Istria",
+        "area": "West coast",
+        "sub_area": "Istra",
+        "place": "Buje (Buie)",
+        "settlment": "BAREDINE",
+        "settlement_id": 7171
+        }
+        },
+        {
+        "id": 19203,
+        "type": "place",
+        "title": "Istria",
+        "subtitle": "Istria, Istra, Poreč (Parenzo)",
+        "terms": {
+        "destination": "Istria",
+        "area": null,
+        "sub_area": "Istra",
+        "place": "Poreč (Parenzo)",
+        "settlment": "DEKLIĆI",
+        "settlement_id": 19203
+        }
+        },
+        {
+        "id": 21507,
+        "type": "place",
+        "title": "Istria",
+        "subtitle": "Istria, Istra, Poreč (Parenzo)",
+        "terms": {
+        "destination": "Istria",
+        "area": null,
+        "sub_area": "Istra",
+        "place": "Poreč (Parenzo)",
+        "settlment": "SVETI LOVREČ PAZENATIČKI",
+        "settlement_id": 21507
+        }
+        }
+]
+searchWidget(testObject);
 //test part
 
-//select list option
+//select list item and display in widget
 function selectItem(){
 const item = document.querySelectorAll('.item');
 item.forEach(element => {
     element.addEventListener('click',()=>{
-        let text = element.dataset.original;
+        //add datasets to selected option in widget
+        optionInput.dataset.id = element.dataset.id;
         optionInput.dataset.type = element.dataset.type;
-        switch (optionInput.dataset.type){
-            case 'Place':
+        //add datasets to selected option in widget
+        switch (optionInput.dataset.type.toLowerCase()){
+            case 'place':
                 typeImg2 = '<img src="./SVG/place.svg" alt=""></img>';
                 break;
-            case 'House':
+            case 'house':
                 typeImg2 = '<img src="./SVG/house.svg" alt=""></img>';
                 break;
-            case 'Apartment':
+            case 'apartment':
                 typeImg2 = '<img src="./SVG/apartment.svg" alt=""></img>';
                 break;
-            case 'Villa':
+            case 'villa':
                 typeImg2 = '<img src="./SVG/villa.svg" alt=""></img>';
                 break;
             default:
                 typeImg2 = '<img src="./SVG/place.svg" alt=""></img>';
         }
-        optionInput.innerHTML = (`${typeImg2}`+`${text}`);
+        //display selected option in widget
+        optionInput.innerHTML = (`${typeImg2}`+`${element.dataset.title}`);
         clear.classList.add('show-transition');
         clear.classList.add('show-block');
+        //display selected option in widget
     })
 });
 }
 selectItem();
-//select list option
+//select list item and display in widget
+
+
